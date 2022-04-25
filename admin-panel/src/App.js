a701cb71829dd0f145b4e805f47638e908b9469e
ilir pllana <ilirpllana95@gmail.com>
2022-04-25 16:47:30 -0400

    finished with login page

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -9,14 +9,22 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/login/Login";
 
 function App() {
+  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin;
   return (
     <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        { admin && (
+          <>
+        <Topbar />
+        <div className="container">
+          <Sidebar />
+        
           <Route exact path="/">
             <Home />
           </Route>
@@ -38,8 +46,11 @@ function App() {
           <Route path="/newproduct">
             <NewProduct />
           </Route>
-        </Switch>
+        
       </div>
+      </>
+          )}
+      </Switch>
     </Router>
   );
 }

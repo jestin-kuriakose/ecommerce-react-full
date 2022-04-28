da2cd229c2de9a0193f5f7b10bb4bf6192f54aec
ilir pllana <ilirpllana95@gmail.com>
2022-04-27 20:11:16 -0400

    cx widget fix

--- admin-panel/src/components/widgetLg/WidgetLg.jsx
+++ admin-panel/src/components/widgetLg/WidgetLg.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 import "./widgetLg.css";
 
 export default function WidgetLg() {
   const Button = ({ type }) => {
     return <button className={"widgetLgButton " + type}>{type}</button>;
   };
+  const [orders, setOrders] = useState([])
+
+  useEffect(()=> {
+    const getUsers = async () => {
+      try{
+        const res = await userRequest.get("users/?new=true");
+        setOrders(res.data)
+      } catch {}
+    }
+    getUsers()
+  }, [])
   return (
     <div className="widgetLg">
       <h3 className="widgetLgTitle">Latest transactions</h3>
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMDliNmU0MjUyZDEzNDZmZDdjMzhjZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MDEwNDY3NywiZXhwIjoxNjUwMzYzODc3fQ.tZJ4TpskYEaBZSoIiJr4eUDRmHeHovGMQroopQxS2LM";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
